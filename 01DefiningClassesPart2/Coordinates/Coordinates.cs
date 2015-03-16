921afc848d170a113dc3589236affe9a9a0068ca
Etonchev <emil.krassimirov@gmail.com>
2015-03-16 18:31:43 +0200

    -

---
+++ 01DefiningClassesPart2/Coordinates/Coordinates.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public struct Point3D
+{
+    private double coordinateX;
+    private double coordinateY;
+    private double coordinateZ;
+    private static readonly Point3D start = new Point3D(0, 0, 0);
+
+    public double CoordinateX
+    { get; set; }
+    public double CoordinateY
+    { get; set; }
+    public double CoordinateZ
+    { get; set; }
+    public Point3D(double x, double y, double z) : this()
+    {
+        this.CoordinateX = x;
+        this.CoordinateY = y;
+        this.CoordinateZ = z;
+    }
+    public override string ToString()
+    {
+        return String.Format("{0}, {1}, {2}", CoordinateX, CoordinateY, CoordinateZ);
+    }
+}
+
+static class Calcualte
+{
+    public static Point3D calculateDistance (Point3D first, Point3D second)
+    {
+        Point3D result = new Point3D();
+        result.CoordinateX = first.CoordinateX - second.CoordinateX;
+        result.CoordinateY = first.CoordinateY - second.CoordinateY;
+        result.CoordinateZ = first.CoordinateZ - second.CoordinateZ;
+        return result;
+    }
+}
+
+public class Test
+{
+    static void Main()
+    {
+        Point3D pointOne = new Point3D(3, 3, 3);
+        Point3D pointTwo = new Point3D(2, 2, 2);
+        Console.WriteLine(Calcualte.calculateDistance(pointOne, pointTwo).ToString());
+    }
+}
---
+++ 01DefiningClassesPart2/GenericList/GenericList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+public class GenericList<T>
+    where T : IComparable
+{
+    private T[] array;
+    private int index;
+
+    public GenericList(int size)
+    {
+        array = new T[size];
+        this.index = 0;
+    }
+    public void Add(T element)
+    {
+        if(this.index == this.array.Length - 1)
+        {
+            Console.WriteLine("Array is full !");
+        }
+        else
+        {
+            array[index] = element;
+            this.index++;
+        }
+    }
+    public T this[int position]
+    {
+        get
+        {
+            return this.array[position];
+        }
+        set
+        {
+            this.array[position] = value;
+        }
+    }
+    public void RemoveAt(int position)
+    {
+        if (position < 0 || position > index)
+        {
+            Console.WriteLine("Doesn't exist !");
+        }
+        else
+        {
+            T[] result = new T[this.array.Length - 1];
+            this.index--;
+            Array.Copy(this.array, 0, result, 0, position);
+            Array.Copy(this.array, position + 1, result, 0, this.array.Length - 1 - position);
+            this.array = result;
+        }
+    }
+    public void InsertAt(T element, int position)
+    {
+        T[] result = new T[this.array.Length + 1];
+        this.index++;
+        Array.Copy(this.array, 0, result, 0, position);
+        result[position] = element;
+        Array.Copy(this.array, position, result, position + 1, this.array.Length - position);
+        this.array = result;
+    }
+    public void Clear()
+    {
+        int sizeTemp = this.array.Length;
+        array = new T[sizeTemp];
+        index = 0;
+    }
+    public int Find(T element)
+    {
+        return Array.IndexOf(this.array, element);
+    }
+    public override string ToString()
+    {
+        string result = String.Empty;
+        for (int i = 0; i < this.index; i++)
+        {
+            result += this.array[i] + " ";
+        }
+        return result;
+    }
+    
+    public void Expand()
+    {
+        T[] result = new T[this.array.Length * 2];
+        Array.Copy(this.array, result, this.array.Length);
+        this.array = result;
+    }
+    public T Min()
+    {
+        T min = this.array[0];
+        for (int i = 0; i < index; i++)
+        {
+            if (min.CompareTo(this.array[i]) > 0)
+            {
+                min = this.array[i];
+            }
+        }
+
+        return min;
+    }
+    public T Max()
+    {
+        T max = this.array[0];
+        for (int i = 0; i < index; i++)
+        {
+            if (max.CompareTo(this.array[i]) < 0)
+            {
+                max = this.array[i];
+            }
+        }
+
+        return max;
+    }
+}
+
+public class Test
+{
+    static void Main()
+    {
+        GenericList<int> array = new GenericList<int>(5);
+        array.Add(1);
+        array.InsertAt(2, 1);
+        array.RemoveAt(1);
+        array.Add(10);
+        Console.WriteLine(array.ToString());
+        Console.WriteLine(array.Min());
+        Console.WriteLine(array.Max());
+    }
+}
