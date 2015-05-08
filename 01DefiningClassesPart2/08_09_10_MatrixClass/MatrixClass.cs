5059b644b618272ae3246db0ea2c53b707399171
Etonchev <emil.krassimirov@gmail.com>
2015-05-08 21:21:06 +0300

    -

--- 01DefiningClassesPart2/08_09_10_MatrixClass/MatrixClass.cs
+++ 01DefiningClassesPart2/08_09_10_MatrixClass/MatrixClass.cs
@@ -22,55 +22,55 @@ class MatrixClass<T>
             Matrix[row, column] = value;
         }
     }
-    public static MatrixClass<double> operator +(MatrixClass<double> m1, MatrixClass<double> m2)
+    public static MatrixClass<T> operator +(MatrixClass<T> m1, MatrixClass<T> m2)
     {
         if (m1.Size != m2.Size)
         {
             throw new Exception("Can't add matrix with different dimensions");
         }
 
-        MatrixClass<double> result = new MatrixClass<double>(m1.Size);
+        MatrixClass<T> result = new MatrixClass<T>(m1.Size);
         for (int row = 0; row < result.Size; row++)
         {
             for (int column = 0; column < result.Size; column++)
             {
-                result[row, column] = m1.Matrix[row, column] + m2.Matrix[row, column];
+                result[row, column] = (dynamic)m1.Matrix[row, column] + m2.Matrix[row, column];
             }
         }
 
         return result;
     }
-    public static MatrixClass<double> operator -(MatrixClass<double> m1, MatrixClass<double> m2)
+    public static MatrixClass<T> operator -(MatrixClass<T> m1, MatrixClass<T> m2)
     {
         if (m1.Size != m2.Size)
         {
             throw new Exception("Can't subtract matrix with different dimensions");
         }
 
-        MatrixClass<double> result = new MatrixClass<double>(m1.Size);
+        MatrixClass<T> result = new MatrixClass<T>(m1.Size);
         for (int row = 0; row < result.Size; row++)
         {
             for (int column = 0; column < result.Size; column++)
             {
-                result[row, column] = m1.Matrix[row, column] - m2.Matrix[row, column];
+                result[row, column] = (dynamic)m1.Matrix[row, column] - m2.Matrix[row, column];
             }
         }
 
         return result;
     }
-    public static MatrixClass<double> operator *(MatrixClass<double> m1, MatrixClass<double> m2)
+    public static MatrixClass<T> operator *(MatrixClass<T> m1, MatrixClass<T> m2)
     {
         if (m1.Size != m2.Size)
         {
             throw new Exception("Can't multiply matrix with different dimensions");
         }
 
-        MatrixClass<double> result = new MatrixClass<double>(m1.Size);
+        MatrixClass<T> result = new MatrixClass<T>(m1.Size);
         for (int row = 0; row < result.Size; row++)
         {
             for (int column = 0; column < result.Size; column++)
             {
-                result[row, column] = m1.Matrix[row, column] * m2.Matrix[row, column];
+                result[row, column] = (dynamic)m1.Matrix[row, column] * m2.Matrix[row, column];
             }
         }
 
@@ -82,9 +82,9 @@ public class MatrixTest
 {
     public static void Main()
     {
-        Console.WriteLine("Enter matrix size: ");
-        int size = Convert.ToInt32(Console.ReadLine());
-        MatrixClass<double> firstMatrix = new MatrixClass<double>(size);
+        Console.Write("Enter first matrix size: ");
+        int firstSize = Convert.ToInt32(Console.ReadLine());
+        MatrixClass<double> firstMatrix = new MatrixClass<double>(firstSize);
         for (int row = 0; row < firstMatrix.Size; row++)
         {
             for (int column = 0; column < firstMatrix.Size; column++)
@@ -94,7 +94,9 @@ public class MatrixTest
             }
         }
 
-        MatrixClass<double> secondMatrix = new MatrixClass<double>(size);
+        Console.Write("Enter second matrix size: ");
+        int secondtSize = Convert.ToInt32(Console.ReadLine());
+        MatrixClass<double> secondMatrix = new MatrixClass<double>(secondtSize);
         for (int row = 0; row < firstMatrix.Size; row++)
         {
             for (int column = 0; column < secondMatrix.Size; column++)
@@ -105,5 +107,14 @@ public class MatrixTest
         }
 
         MatrixClass<double> result = firstMatrix + secondMatrix;
+        for (int row = 0; row < firstMatrix.Size; row++)
+        {
+            for (int column = 0; column < secondMatrix.Size; column++)
+            {
+                Console.Write("{0, 2} ", result[row, column]);
+            }
+
+            Console.WriteLine();
+        }
     }
 }
