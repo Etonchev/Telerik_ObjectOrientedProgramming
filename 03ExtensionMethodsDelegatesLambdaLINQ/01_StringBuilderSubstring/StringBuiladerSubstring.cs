0814ec08427202f17c08edf1c12a08cae62beecc
Etonchev <emil.krassimirov@gmail.com>
2015-05-10 10:54:30 +0300

    -

---
+++ 03ExtensionMethodsDelegatesLambdaLINQ/01_StringBuilderSubstring/StringBuiladerSubstring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+public static class StringBuiladerSubstring
+{
+    public static StringBuilder Substring(this StringBuilder input, int index, int length)
+    {
+        StringBuilder substring = new StringBuilder(input.ToString(), index, length, input.Capacity);
+        return substring;
+    }
+}
+
+public class Test
+{
+    public static void Main()
+    {
+        StringBuilder text = new StringBuilder("This is a test");
+        StringBuilder substring = text.Substring(5, text.Length - 5);
+        Console.WriteLine(substring);
+    }
+}
---
+++ 03ExtensionMethodsDelegatesLambdaLINQ/02_IENumerableExtensions/IENumerableExtensions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class IENumerableExtensions<T>
+{
+    public static T Sum<T>(this IEnumerable<T> elements)
+    {
+        if (elements == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        T sum = default(T);
+        foreach (var item in elements)
+        {
+            sum += (dynamic)item;
+        }
+
+        return sum;
+    }
+
+    public static T Product<T>(this IEnumerable<T> elements)
+    {
+        if (elements == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        T product = (dynamic)1;
+        foreach (var item in elements)
+        {
+            product *= (dynamic)item;
+        }
+
+        return product;
+    }
+
+    public static T Average<T>(this IEnumerable<T> elements)
+    {
+        if (elements == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        T sum = default(T);
+        int elementsCount = 0;
+        foreach (var item in elements)
+        {
+            elementsCount++;
+            sum += (dynamic)item;
+        }
+        T average = (dynamic)sum / elementsCount;
+
+        return average;
+    }
+
+    public static T Min<T>(this IEnumerable<T> elements)
+    {
+        if (elements == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        T min = elements.First();
+        foreach (var item in elements)
+        {
+            if ((dynamic)item < min)
+            {
+                min = item;
+            }
+        }
+
+        return min;
+    }
+
+    public static T Max<T>(this IEnumerable<T> elements)
+    {
+        if (elements == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        T max = elements.First();
+        foreach (var item in elements)
+        {
+            if ((dynamic)item > max)
+            {
+                max = item;
+            }
+        }
+
+        return max;
+    }
+}
